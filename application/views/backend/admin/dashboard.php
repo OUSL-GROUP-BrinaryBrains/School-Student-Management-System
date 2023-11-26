928ef337c56920eb37a37fca08bc15539816d516
mtmzakir <mtm.zakir@gmail.com>
2023-11-27 05:04:46 +0530

    Admin Update6

--- application/views/backend/admin/dashboard.php
+++ application/views/backend/admin/dashboard.php
@@ -1,123 +1,59 @@
-<div class="row" style="color:#001911 ;font-family: system-ui;font-size:14px;">
-
-
+<div class="row">
     <div class="col-md-12">
         <div class="row" style="color:#001911 ;font-family: system-ui;font-size:14px;">
 
-
             <div class="col-md-4">
-
-                <div class="tile-stats tile-red" style="height:180px">
+                <div class="tile-stats tile-red">
                     <div class="icon"><i class="entypo-graduation-cap"></i></div>
                     <div class="num" data-start="0" data-end="<?php echo $this->db->count_all('student'); ?>" data-postfix="" data-duration="1500" data-delay="0">0</div>
-
                     <h3><?php echo ('Total Students'); ?></h3>
-
                 </div>
-
             </div>
-            <div class="col-md-4">
 
-                <div class="tile-stats tile-blue" style="height:180px">
+            <div class="col-md-4">
+                <div class="tile-stats tile-blue">
                     <div class="icon"><i class="entypo-users"></i></div>
                     <div class="num" data-start="0" data-end="<?php echo $this->db->count_all('teacher'); ?>" data-postfix="" data-duration="800" data-delay="0">0</div>
-
                     <h3><?php echo ('Total Teachers'); ?></h3>
-
                 </div>
-
             </div>
 
             <div class="col-md-4">
-
-                <div class="tile-stats tile-green" style="height:180px">
+                <div class="tile-stats tile-green">
                     <div class="icon"><i class="entypo-calendar"></i></div>
                     <?php
-                    $check    =    array('date' => date('Y-m-d'), 'status' => '1');
+                    $check = array('date' => date('Y-m-d'), 'status' => '1');
                     $query = $this->db->get_where('attendance', $check);
-                    $present_today        =    $query->num_rows();
-                    ?>
+                    $present_today        =    $query->num_rows(); ?>
                     <div class="num" data-start="0" data-end="<?php echo $present_today; ?>" data-postfix="" data-duration="500" data-delay="0">0</div>
-
                     <h3><?php echo ('Today Attendance'); ?></h3>
-
                 </div>
-
             </div>
 
-
             <div class="col-md-4">
-
-                <div class="tile-stats tile-purple" style="height:180px">
+                <div class="tile-stats tile-purple">
                     <div class="icon"><i class="entypo-user"></i></div>
                     <div class="num" data-start="0" data-end="<?php echo $this->db->count_all('parent'); ?>" data-postfix="" data-duration="500" data-delay="0">0</div>
-
                     <h3><?php echo ('Total Parents'); ?></h3>
-
                 </div>
-
             </div>
 
             <div class="col-md-4">
-
-                <div class="tile-stats tile-black" style="height:180px">
+                <div class="tile-stats tile-black">
                     <div class="icon"><i class="entypo-flow-tree"></i></div>
                     <div class="num" data-start="0" data-end="<?php echo $this->db->count_all('class'); ?>" data-postfix="" data-duration="500" data-delay="0">0</div>
-
                     <h3><?php echo ('Available Classes'); ?></h3>
-
                 </div>
-
             </div>
 
             <div class="col-md-4">
-
-                <div class="tile-stats tile-aqua" style="height:180px">
+                <div class="tile-stats tile-aqua">
                     <div class="icon"><i class="entypo-alert"></i></div>
                     <div class="num" data-start="0" data-end="<?php echo $this->db->count_all('noticeboard'); ?>" data-postfix="" data-duration="500" data-delay="0">0</div>
-
                     <h3><?php echo ('Notices'); ?></h3>
-
                 </div>
-
             </div>
 
         </div>
     </div>
-
-    <script>
-        $(document).ready(function() {
-
-            var calendar = $('#notice_calendar');
-
-            $('#notice_calendar').fullCalendar({
-                header: {
-                    left: 'title',
-                    right: 'today prev,next'
-                },
-
-                //defaultView: 'basicWeek',
-
-                editable: false,
-                firstDay: 1,
-                height: 450,
-                width: 300,
-                droppable: false,
-
-                events: [
-                    <?php
-                    $notices    =    $this->db->get('noticeboard')->result_array();
-                    foreach ($notices as $row) :
-                    ?> {
-                            title: "<?php echo $row['notice_title']; ?>",
-                            start: new Date(<?php echo date('Y', $row['create_timestamp']); ?>, <?php echo date('m', $row['create_timestamp']) - 1; ?>, <?php echo date('d', $row['create_timestamp']); ?>),
-                            end: new Date(<?php echo date('Y', $row['create_timestamp']); ?>, <?php echo date('m', $row['create_timestamp']) - 1; ?>, <?php echo date('d', $row['create_timestamp']); ?>)
-                        },
-                    <?php
-                    endforeach
-                    ?>
-
-                ]
-            });
-        });
-    </script>
+</div>
