20af8d6b32f9f8db09bef1d6a55d612ea86ff9df
mtmzakir <mtm.zakir@gmail.com>
2023-11-29 03:43:51 +0530

    Removed Get_Phrase from Controller

--- application/controllers/Parents.php
+++ application/controllers/Parents.php
@@ -121,36 +121,6 @@ class Parents extends CI_Controller
         $this->load->view('backend/index', $page_data);
         
     }
-    
-    /* private messaging */
-
-    function message($param1 = 'message_home', $param2 = '', $param3 = '') {
-        if ($this->session->userdata('parent_login') != 1)
-            redirect(base_url(), 'refresh');
-
-        if ($param1 == 'send_new') {
-            $message_thread_code = $this->crud_model->send_new_private_message();
-            $this->session->set_flashdata('flash_message', get_phrase('message_sent!'));
-            redirect(base_url() . 'index.php?parents/message/message_read/' . $message_thread_code, 'refresh');
-        }
-
-        if ($param1 == 'send_reply') {
-            $this->crud_model->send_reply_message($param2);  //$param2 = message_thread_code
-            $this->session->set_flashdata('flash_message', get_phrase('message_sent!'));
-            redirect(base_url() . 'index.php?parents/message/message_read/' . $param2, 'refresh');
-        }
-
-        if ($param1 == 'message_read') {
-            $page_data['current_message_thread_code'] = $param2;  // $param2 = message_thread_code
-            $this->crud_model->mark_thread_messages_read($param2);
-        }
-
-        $page_data['message_inner_page_name']   = $param1;
-        $page_data['page_name']                 = 'message';
-        $page_data['page_title']                = 'Messages';
-        $this->load->view('backend/index', $page_data);
-    }
-    
     /******MANAGE OWN PROFILE AND CHANGE PASSWORD***/
     function manage_profile($param1 = '', $param2 = '', $param3 = '')
     {
@@ -162,7 +132,7 @@ class Parents extends CI_Controller
             
             $this->db->where('parent_id', $this->session->userdata('parent_id'));
             $this->db->update('parent', $data);
-            $this->session->set_flashdata('flash_message', get_phrase('account_updated'));
+            $this->session->set_flashdata('flash_message', 'account_updated');
             redirect(base_url() . 'index.php?parents/manage_profile/', 'refresh');
         }
         if ($param1 == 'change_password') {
@@ -178,9 +148,9 @@ class Parents extends CI_Controller
                 $this->db->update('parent', array(
                     'password' => $data['new_password']
                 ));
-                $this->session->set_flashdata('flash_message', get_phrase('password_updated'));
+                $this->session->set_flashdata('flash_message', 'Account Password Updated!');
             } else {
-                $this->session->set_flashdata('flash_message', get_phrase('password_mismatch'));
+                $this->session->set_flashdata('flash_message', 'Password Mismatch!');
             }
             redirect(base_url() . 'index.php?parents/manage_profile/', 'refresh');
         }
--- application/controllers/Student.php
+++ application/controllers/Student.php
@@ -147,37 +147,6 @@ class Student extends CI_Controller
         $this->load->view('backend/index', $page_data);
         
     }
-    
-    
-    /* private messaging */
-
-    function message($param1 = 'message_home', $param2 = '', $param3 = '') {
-        if ($this->session->userdata('student_login') != 1)
-            redirect(base_url(), 'refresh');
-
-        if ($param1 == 'send_new') {
-            $message_thread_code = $this->crud_model->send_new_private_message();
-            $this->session->set_flashdata('flash_message', get_phrase('message_sent!'));
-            redirect(base_url() . 'index.php?student/message/message_read/' . $message_thread_code, 'refresh');
-        }
-
-        if ($param1 == 'send_reply') {
-            $this->crud_model->send_reply_message($param2);  //$param2 = message_thread_code
-            $this->session->set_flashdata('flash_message', get_phrase('message_sent!'));
-            redirect(base_url() . 'index.php?student/message/message_read/' . $param2, 'refresh');
-        }
-
-        if ($param1 == 'message_read') {
-            $page_data['current_message_thread_code'] = $param2;  // $param2 = message_thread_code
-            $this->crud_model->mark_thread_messages_read($param2);
-        }
-
-        $page_data['message_inner_page_name']   = $param1;
-        $page_data['page_name']                 = 'message';
-        $page_data['page_title']                = 'Messages';
-        $this->load->view('backend/index', $page_data);
-    }
-    
     /******MANAGE OWN PROFILE AND CHANGE PASSWORD***/
     function manage_profile($param1 = '', $param2 = '', $param3 = '')
     {
@@ -190,7 +159,7 @@ class Student extends CI_Controller
             $this->db->where('student_id', $this->session->userdata('student_id'));
             $this->db->update('student', $data);
             move_uploaded_file($_FILES['userfile']['tmp_name'], 'uploads/student_image/' . $this->session->userdata('student_id') . '.jpg');
-            $this->session->set_flashdata('flash_message', get_phrase('account_updated'));
+            $this->session->set_flashdata('flash_message', 'account_updated');
             redirect(base_url() . 'index.php?student/manage_profile/', 'refresh');
         }
         if ($param1 == 'change_password') {
@@ -206,9 +175,9 @@ class Student extends CI_Controller
                 $this->db->update('student', array(
                     'password' => $data['new_password']
                 ));
-                $this->session->set_flashdata('flash_message', get_phrase('password_updated'));
+                $this->session->set_flashdata('flash_message', 'Account Password Updated!');
             } else {
-                $this->session->set_flashdata('flash_message', get_phrase('password_mismatch'));
+                $this->session->set_flashdata('flash_message', 'Password Mismatch!');
             }
             redirect(base_url() . 'index.php?student/manage_profile/', 'refresh');
         }
