b7b65f3b4d1a6672703cef5cde61f0845d061d86
SAFRAN\nmsaf <nmsafranmhd@gmail.com>
2023-11-30 22:48:24 +0530

    HeaderFooterUpdated

--- application/views/backend/header.php
+++ application/views/backend/header.php
@@ -2,7 +2,7 @@
 	<!-- Raw Links -->
 	<div class="col-md-12 col-sm-12 clearfix">
 		<h3 style="margin-bottom:0px; margin-top:0px;font-weight: 500; color:#ffa800; font-family: system-ui;"><?php echo "Welcome," ?> <?php echo  $this->session->userdata('name'); ?></h3>
-		<ul class="list-inline links-list pull-left" style="margin-bottom:0px; margin-top:0px;" >
+		<ul class="list-inline links-list pull-left" style="margin-bottom:0px; margin-top:0px;">
 			<!-- Profile Type -->
 			<li class="dropdown language-selector">
 				<h3 style="font-weight: 400; font-size:15px;color:#008055;">
@@ -10,17 +10,15 @@
 				</h3>
 			</li>
 		</ul>
-		<ul class="list-inline links-list pull-right" style="margin-bottom:0px; margin-top:0px;" >
+		<ul class="list-inline links-list pull-right" style="margin-bottom:0px; margin-top:0px;">
 			<!-- Logout Button -->
 			<li>
-				<a href="<?php echo base_url(); ?>index.php?login/logout" 
-				style="font-weight: 400; font-size:15px;color:#008055;">
-					Logout 
+				<a href="<?php echo base_url(); ?>index.php?login/logout" style="font-weight: 400; font-size:15px;color:#008055;">
+					Logout
 					<i class="entypo-logout right"></i>
 				</a>
 			</li>
 		</ul>
 	</div>
 </div>
-
 <hr style="margin-bottom:0px; margin-top:0px;  border-bottom: 0.5px solid #ffa800; width: 100%" />
